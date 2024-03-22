a1e0c8095bfb9303bd38a62caad09b3130fe9d22
gm2211 <gmecocci@outlook.com>
2024-03-22 01:28:47 -0400

    fix

--- src/main/python/boat/telemetry/nav_params_recorder.py
+++ src/main/python/boat/telemetry/nav_params_recorder.py
@@ -1,8 +1,20 @@
 from api.objects.nav_params.nav_params import NavParams
 from api.objects.units.angle import Angle
+from api.objects.units.gps_coord import GPSCoord
+from api.objects.units.speed import Speed
 
 
 class NavParamsRecorder:
+
+    def __init__(self):
+        self.cur_nav_params = NavParams(
+            heading=Angle(0),
+            rudder_angle=Angle(0),
+            position=GPSCoord(0, 0),
+            speed=Speed(0),
+            true_wind_angle=Angle(0),
+            sail_trim=Angle(0)
+        )
 
     def record_rudder_angle(self, angle: Angle) -> None:
         pass
@@ -14,4 +26,4 @@ class NavParamsRecorder:
         pass
 
     def get_cur_nav_params(self) -> NavParams:
-        pass
+        return self.cur_nav_params
