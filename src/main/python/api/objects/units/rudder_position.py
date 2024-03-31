43d23131fb8588e7bf3d91258f7be5837253ccf8
gm2211 <gmecocci@outlook.com>
2024-03-31 02:20:44 -0400

    fix

--- src/main/python/api/objects/units/rudder_position.py
+++ src/main/python/api/objects/units/rudder_position.py
@@ -9,10 +9,10 @@ class RudderPosition(NamedTuple):
 
     @staticmethod
     def left(percent: Percent) -> 'RudderPosition':
-        remapped = remap(percent, 0, 100, 50, 100)
+        remapped = remap(percent.value, 0, 100, 50, 100)
         return RudderPosition(Percent(remapped))
 
     @staticmethod
     def right(percent: Percent) -> 'RudderPosition':
-        remapped = remap(percent, 0, 100, 50, 0)
+        remapped = remap(percent.value, 0, 100, 50, 0)
         return RudderPosition(Percent(remapped))
