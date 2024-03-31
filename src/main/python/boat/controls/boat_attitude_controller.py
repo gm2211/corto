d70afeeb89d21ae93c5e25aedacec84ebdab7ef0
gm2211 <gmecocci@outlook.com>
2024-03-31 02:32:02 -0400

    wip

--- src/main/python/boat/controls/boat_attitude_controller.py
+++ src/main/python/boat/controls/boat_attitude_controller.py
@@ -47,8 +47,5 @@ if __name__ == "__main__":
     b.set_attitude(BoatAttitude(RudderPosition.right(Percent(70)), Percent(0)))
 
     input("center")
-    b.set_attitude(BoatAttitude(RudderPosition.left(Percent(0)), Percent(0)))
-    b.set_attitude(BoatAttitude(RudderPosition.left(Percent(0)), Percent(0)))
-    b.set_attitude(BoatAttitude(RudderPosition.left(Percent(0)), Percent(0)))
-    b.set_attitude(BoatAttitude(RudderPosition.left(Percent(0)), Percent(0)))
-    b.set_attitude(BoatAttitude(RudderPosition.left(Percent(0)), Percent(0)))
+    b.set_attitude(BoatAttitude(RudderPosition(Percent(0)), Percent(0)))
+
