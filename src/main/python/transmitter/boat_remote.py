d30290f2e5226436a83e64cfff253674d9a0cddf
gm2211 <gmecocci@outlook.com>
2024-03-31 22:11:00 -0400

    Fix

--- src/main/python/transmitter/boat_remote.py
+++ src/main/python/transmitter/boat_remote.py
@@ -4,7 +4,7 @@ import busio
 from api.objects.commands.set_sail import SetSail
 from api.objects.commands.set_throttle import SetThrottle
 from api.objects.commands.turn_rudder import TurnRudder
-from api.objects.units.angle import Angle
+from api.objects.units.percent import Percent
 from lora.radio import Radio
 
 
@@ -16,29 +16,29 @@ class BoatRemote:
         self.radio: Radio = radio
 
     def run_loop(self) -> None:
-        rudder = 0
-        sail = 0
+        rudder = 50
+        sail = 50
         throttle = 0
 
         while True:
             command = input("Command: ")
             if command == "l":
-                rudder = max(-100, rudder - 1) % 360
+                rudder = max(0, rudder - 1)
                 self.__show(f"left {rudder}")
-                self.__send_rudder(rudder)
+                self.__send_rudder(Percent(rudder))
             elif command == "r":
-                rudder = min(100, rudder + 1) % 360
+                rudder = min(100, rudder + 1)
                 self.__show(f"right {rudder}")
-                self.__send_rudder(rudder)
+                self.__send_rudder(Percent(rudder))
             elif command == "u":
                 rudder = 0
-                sail = min(100, sail + 1) % 360
+                sail = min(100, sail + 1)
                 self.__show(f"up {sail}")
-                self.__send_sail(sail)
+                self.__send_sail(Percent(sail))
             elif command == "d":
-                sail = max(-100, sail - 1) % 360
+                sail = max(0, sail - 1)
                 self.__show(f"down {sail}")
-                self.__send_sail(sail)
+                self.__send_sail(Percent(sail))
             elif command == "w":
                 throttle = min(100, throttle + 10)
                 self.__show(f"throttle up {throttle}")
@@ -48,12 +48,12 @@ class BoatRemote:
                 self.__show(f"throttle down {throttle}")
                 self.__send_throttle(throttle)
 
-    def __send_rudder(self, rudder):
-        serialized = TurnRudder(Angle(rudder)).serialize_for_lora()
+    def __send_rudder(self, rudder: Percent):
+        serialized = TurnRudder(rudder).serialize_for_lora()
         self.radio.send(serialized)
 
-    def __send_sail(self, sail):
-        serialized = SetSail(Angle(sail)).serialize_for_lora()
+    def __send_sail(self, sail: Percent):
+        serialized = SetSail(sail).serialize_for_lora()
         self.radio.send(serialized)
 
     def __send_throttle(self, throttle):
